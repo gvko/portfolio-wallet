5f7e982b172f79e6fb7c8076d3a11ec6a2e6e347
Galin Kostov <dev@galin.cc>
2022-12-23 13:10:57 +0100

    Move rust code to /server subdir

---
+++ server/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
+}
