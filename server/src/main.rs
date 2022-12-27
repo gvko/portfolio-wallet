3526cc290eb474696365345910dac008f012446b
Galin Kostov <dev@galin.cc>
2022-12-27 15:59:32 +0100

    Refactor out logic for making external api request & parsing the result into separate function

--- server/src/main.rs
+++ server/src/main.rs
@@ -41,26 +41,19 @@ struct UserCoinBalance {
     logo: String,
 }
 
-#[get("/coins/<wallet_address>")]
-async fn get_coins(wallet_address: &str) -> Result<Json<Vec<UserCoinBalance>>, Error> {
-    let api_base_url = dotenv!("API_URL");
-    let api_key = dotenv!("API_KEY");
-    let api_url = format!("{}/{}", api_base_url, api_key);
-
-    let wallet_address = wallet_address.to_string();
-
+async fn make_request<T>(api_url: &String, endpoint: String, params: String) -> T where T: for<'a> Deserialize<'a> {
     let data = json!({
         "jsonrpc": "2.0",
-        "method": "alchemy_getTokenBalances",
+        "method": endpoint,
         "headers": {
             "Content-Type": "application/json",
         },
-        "params": [format!("{}", wallet_address)]
+        "params": [params]
     });
 
     let client = Client::new();
     let res = client
-        .post(&api_url)
+        .post(api_url)
         .header(header::CONTENT_TYPE, "application/json")
         .json(&data)
         .send()
@@ -68,35 +61,24 @@ async fn get_coins(wallet_address: &str) -> Result<Json<Vec<UserCoinBalance>>, E
 
     let result = res.unwrap();
     let result = result.json::<serde_json::Value>().await.unwrap();
-    let result: TokenBalancesApiResult = serde_json::from_value(result["result"].clone()).unwrap();
+    let result: T = serde_json::from_value(result["result"].clone()).unwrap();
+    result
+}
 
+#[get("/coins/<wallet_address>")]
+async fn get_coins(wallet_address: &str) -> Result<Json<Vec<UserCoinBalance>>, Error> {
+    let api_base_url = dotenv!("API_URL");
+    let api_key = dotenv!("API_KEY");
+    let api_url = format!("{}/{}", api_base_url, api_key);
+    let wallet_address = wallet_address.to_string();
+
+    let result: TokenBalancesApiResult = make_request(&api_url, "alchemy_getTokenBalances".to_string(), format!("{}", wallet_address)).await;
     let mut user_coins: Vec<UserCoinBalance> = vec![];
 
     for token_balance in &result.tokenBalances {
-        let data = json!({
-            "jsonrpc": "2.0",
-            "method": "alchemy_getTokenMetadata",
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "params": [format!("{}", token_balance.contractAddress)]
-        });
+        let result: TokenInfoApiResult = make_request(&api_url, "alchemy_getTokenMetadata".to_string(), format!("{}", token_balance.contractAddress)).await;
+        let logo = result.logo.clone().unwrap_or("null".to_string());
 
-        let client = Client::new();
-        let res = client
-            .post(&api_url)
-            .header(header::CONTENT_TYPE, "application/json")
-            .json(&data)
-            .send()
-            .await;
-
-        let result = res.unwrap();
-        let result = result.json::<serde_json::Value>().await.unwrap();
-        let result = serde_json::from_value::<TokenInfoApiResult>(result["result"].clone())?;
-
-        let logo = result.logo.clone().unwrap_or_else(|| "null".to_string());
-
-        // println!("{}", token_balance.tokenBalance);
         let hexnum = token_balance.tokenBalance.trim_start_matches("0x");
         let balance_as_float = u128::from_str_radix(hexnum, 16).unwrap() as f64;
         let balance = balance_as_float / 10_f64.powi(result.decimals);
