c793d769e17f4925649e6e20b7ba8eb4d0ecc5b1
Galin Kostov <dev@galin.cc>
2022-12-29 10:03:33 +0100

    Move middleware to dedicated mod and dir

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,35 +4,16 @@ extern crate dotenv_codegen;
 
 mod alchemy_api;
 mod coins_service;
+mod middlewares;
 
+use coins_service::*;
+use middlewares::CORS;
 use std::io::Error;
 use dotenv_codegen::dotenv;
 use rocket::{
-    Build, Rocket, Request, Response,
-    http::{Header},
+    Build, Rocket,
     serde::{json::Json},
 };
-use rocket::fairing::{Fairing, Info, Kind};
-use coins_service::*;
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Attaching CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[get("/coins/<wallet_address>")]
 async fn get_coins(wallet_address: &str) -> Result<Json<Vec<UserCoinBalance>>, Error> {
